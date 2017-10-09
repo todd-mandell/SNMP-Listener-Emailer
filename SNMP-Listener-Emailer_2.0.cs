b48828d98da63808e14e0b8e47e6d899722ce120
Todd Mandell <todd.mandell@outlook.com>
2017-10-09 09:19:03 -0400

    Update SNMP-Listener-Emailer_2.0.cs

--- SNMP-Listener-Emailer_2.0.cs
+++ SNMP-Listener-Emailer_2.0.cs
@@ -7,24 +7,32 @@ using System.Net;
 using System.Net.Mail;
 using System.Web;
 using System.IO;
+using System.Diagnostics;
 
 namespace OpenITTools_SNMP_Listener_Emailer
 {
     class Program
-    {  
+    {
+        
         static void Main(string[] args)
         {
+
+            Process [] localByName = Process.GetProcessesByName("snmp*");
+            foreach(Process p in localByName)
+            {
+                 p.Kill();
+            }
+        
             UdpClient listener;
             int port = 162;
-            listener = new UdpClient(port);
             IPEndPoint groupEP;
             byte[] packet = new byte[1024];
-            //done to allow reuse of the socket 
             listener = new UdpClient(port);
             groupEP = new IPEndPoint(IPAddress.Any, port);
             
             while (true)
             {
+
                 Console.WriteLine("Waiting for messages....");
                 packet = listener.Receive(ref groupEP);
                 Console.WriteLine("Processing new message...");
@@ -36,26 +44,30 @@ namespace OpenITTools_SNMP_Listener_Emailer
                     string SNMPPK = packet + " - " + DateTime.Now;
                     string SNMPSubject = "SNMP Alert From " + SNMPEP + " " + SNMPPK;
                     string SNMPBody = "SNMP Alert From " + SNMPEP + " " + SNMPPK + " - " + output2;
+                    string SNMPshortMsg = output2;
                     string path = @"c:\snmp-log.txt";
 
                     if (packet[0] == 0xff)
                     {
                         Console.WriteLine(DateTime.Now + " - Invalid Packet");
+
                         //add the log file and emailer to this part too since invalid packets would be important
                         try
                         {
+
                             // SMTP Stuff Begin
                             SmtpClient client = new SmtpClient();
                             client.Port = 587;
-                            client.Host = "SnmpHostURI";
+                            client.Host = "smtpServerAddress";
                             client.EnableSsl = true;
 
                             client.Timeout = 10000;
                             client.DeliveryMethod = SmtpDeliveryMethod.Network;
                             client.UseDefaultCredentials = false;
-                            client.Credentials = new System.Net.NetworkCredential("SnmpUser", "SnmpPassword");
+                            client.Credentials = new System.Net.NetworkCredential("smtpUser", "smtpPassword");
 
-                            MailMessage mm = new MailMessage("emailFrom", "emailTo", SNMPSubject, SNMPBody);
+
+                            MailMessage mm = new MailMessage("emailFromAddress", "emailToAddress", DateTime.Now + " - SNMP INVALID PACKET", DateTime.Now + " - SNMP INVALID PACKET");
                             mm.BodyEncoding = UTF8Encoding.UTF8;
                             mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
 
@@ -63,6 +75,7 @@ namespace OpenITTools_SNMP_Listener_Emailer
 
                             try
                             {
+
                                 // This text is added only once to the file.
                                 if (!File.Exists(path))
                                 {
@@ -73,6 +86,7 @@ namespace OpenITTools_SNMP_Listener_Emailer
                                         sw.WriteLine("----------------------------------------------------------------------");
                                     }
                                 }
+
                                 // This text is always appended for a successful SNMP Email
                                 using (StreamWriter sw = File.AppendText(path))
                                 {
@@ -84,7 +98,10 @@ namespace OpenITTools_SNMP_Listener_Emailer
                             {
                                 Console.WriteLine("Email Notifier & Log File Failure for Invalid Packet");
                             }
+
+
                             return;
+
                         }
                         catch
                         {
@@ -100,14 +117,16 @@ namespace OpenITTools_SNMP_Listener_Emailer
                         // SMTP Stuff Begin
                         SmtpClient client = new SmtpClient();
                         client.Port = 587;
-                        client.Host = "SnmpHostURI";
+                        client.Host = "snmpServerAddress";
                         client.EnableSsl = true;
+
                         client.Timeout = 10000;
                         client.DeliveryMethod = SmtpDeliveryMethod.Network;
                         client.UseDefaultCredentials = false;
-                        client.Credentials = new System.Net.NetworkCredential("SnmpUser", "SnmpPassword");
+                        client.Credentials = new System.Net.NetworkCredential("smtpUser", "smtpPassword");
 
-                        MailMessage mm = new MailMessage("emailFrom", "emailTo", SNMPSubject, SNMPBody);
+
+                        MailMessage mm = new MailMessage("emailFromAddress", "emailToAddress", SNMPSubject, SNMPshortMsg);
                         mm.BodyEncoding = UTF8Encoding.UTF8;
                         mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
 
@@ -115,6 +134,7 @@ namespace OpenITTools_SNMP_Listener_Emailer
 
                         try
                         {
+
                             // This text is added only once to the file.
                             if (!File.Exists(path))
                             {
@@ -124,7 +144,8 @@ namespace OpenITTools_SNMP_Listener_Emailer
                                     sw.WriteLine("snmp-log.txt Log File Begin");
                                     sw.WriteLine("----------------------------------------------------------------------");
                                 }
-                            } 
+                            }
+                             
                             // This text is always appended for a successful SNMP Email
                             using (StreamWriter sw = File.AppendText(path))
                             {
@@ -136,10 +157,14 @@ namespace OpenITTools_SNMP_Listener_Emailer
                         {
                             Console.WriteLine("Email Notifier & Log File Failure");
                         }
+
                     }
                     catch (Exception EXCEP)
-                    { 
-                        Console.WriteLine(DateTime.Now + " - SNMP Email Failed ----- " + EXCEP + " ----- " + SNMPBody); 
+                    {
+                        
+                        Console.WriteLine(DateTime.Now + " - SNMP Email Failed ----- " + EXCEP + " ----- " + SNMPBody);
+                        
+                        try
                         {
                             
                             // This text is added only once to the file.
