f03f48423867b7a379a9f79d81ae67534b4a80e3
Todd Mandell <todd.mandell@outlook.com>
2018-09-10 07:18:37 -0400

    Update SNMP-Listener-Emailer.cs
    it's becoming messy, but the offline buffering was implemented and secureString was attempted. this ver works as before but new features are not fully ready. any suggestions for refactoring using a function would be appreciated.

--- SNMP-Listener-Emailer.cs
+++ SNMP-Listener-Emailer.cs
@@ -8,6 +8,7 @@ using System.Net.Mail;
 using System.Web;
 using System.IO;
 using System.Diagnostics;
+using System.Security;
 
 namespace OpenITTools_SNMP_Listener_Emailer
 {
@@ -16,13 +17,15 @@ namespace OpenITTools_SNMP_Listener_Emailer
         
         static void Main(string[] args)
         {
+
             try
             {
                 Process.Start("powershell set-service snmp -startuptype disabled");
                 Process.Start("powershell spsv snmp -force");
                 Process.Start("powershell spps snmp -force");
             }
-            catch {
+            catch 
+            {
                 Console.WriteLine("-----SNMP Kill Failed-----");
             }
 
@@ -50,36 +53,52 @@ namespace OpenITTools_SNMP_Listener_Emailer
                     string SNMPSubject = "SNMP Alert From " + SNMPEP + " " + SNMPPK;
                     string SNMPBody = "SNMP Alert From " + SNMPEP + " " + SNMPPK + " - " + output2;
                     string SNMPshortMsg = output2;
-                    string path = @"c:\Users\" + Environment.UserName + "\\snmp-log.txt";
+                    string path = @"c:\snmp-log.txt"; // Users\" + Environment.UserName + "\\snmp-log.txt";
+                    string bufferPath = @"c:\snmp-sendBuffer.txt";
+                    
+
+                    // if invalid packet found do this first
 
                     if (packet[0] == 0xff)
                     {
                         Console.WriteLine(DateTime.Now + " - Invalid Packet");
 
+
+
                         //add the log file and emailer to this part too since invalid packets would be important
                         try
                         {
 
+                            string smtpFile = "c:\\SMTPINFO.txt";
+                            //SecureString gave some difficulties here would like to implement that though
+                            //SecureString ss = new NetworkCredential("", File.ReadLines(smtpFile).Skip(0).Take(1).First());
+                            string SMTPPORT = File.ReadLines(smtpFile).Skip(0).Take(1).First();
+                            string SMTPHOSTURL = File.ReadLines(smtpFile).Skip(1).Take(1).First();
+                            string USERNAME = File.ReadLines(smtpFile).Skip(2).Take(1).First();
+                            string PASSWORD = File.ReadLines(smtpFile).Skip(3).Take(1).First();
+                            string FROMADDRESS = File.ReadLines(smtpFile).Skip(4).Take(1).First();
+                            string TOADDRESS = File.ReadLines(smtpFile).Skip(5).Take(1).First(); 
+
                             // SMTP Stuff Begin for invalid packet
-                            SmtpClient client = new SmtpClient();
-                            client.Port = 587;
-                            client.Host = "SMTP-SERVER";
-                            client.EnableSsl = true;
+                            SmtpClient invalidPacket = new SmtpClient();
+                            invalidPacket.Port = Int32.Parse(SMTPPORT);
+                            invalidPacket.Host = SMTPHOSTURL;
+                            invalidPacket.EnableSsl = true;
 
-                            client.Timeout = 10000;
-                            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                            client.UseDefaultCredentials = false;
-                            client.Credentials = new System.Net.NetworkCredential("SMTP-USERNAME", "SMTP-PW");
+                            invalidPacket.Timeout = 10000;
+                            invalidPacket.DeliveryMethod = SmtpDeliveryMethod.Network;
+                            invalidPacket.UseDefaultCredentials = false;
+                            invalidPacket.Credentials = new System.Net.NetworkCredential(USERNAME, PASSWORD);
 
 
-                        MailMessage mm = new MailMessage("EMAIL-FROM", "EMAIL-TO", DateTime.Now + " - SNMP INVALID PACKET", DateTime.Now + " - SNMP INVALID PACKET");
+                            MailMessage mm = new MailMessage(FROMADDRESS, TOADDRESS, DateTime.Now + " - SNMP INVALID PACKET", DateTime.Now + " - SNMP INVALID PACKET");
                             mm.BodyEncoding = UTF8Encoding.UTF8;
                             mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
                             mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess;
                             mm.DeliveryNotificationOptions = DeliveryNotificationOptions.Delay;
-                            
-                            client.Send(mm);
 
+                            invalidPacket.Send(mm);
+                            
                             try
                             {
 
@@ -104,6 +123,17 @@ namespace OpenITTools_SNMP_Listener_Emailer
                             catch
                             {
                                 Console.WriteLine("Log File Failure for Invalid Packet");
+                                // create and log to the buffer file begin
+                                      if (!File.Exists(bufferPath))
+                                {
+                                    // Create a file to write to.
+                                    using (StreamWriter sw = File.CreateText(bufferPath))
+                                    {
+                                        sw.WriteLine(DateTime.Now + " - SNMP INVALID PACKET");                                        
+                                    }
+                                }
+
+                              
                             }
 
 
@@ -118,22 +148,32 @@ namespace OpenITTools_SNMP_Listener_Emailer
 
                     Console.WriteLine(packet + " - " + DateTime.Now);
 
+                    //try to send the snmp message
                     try
                     {
-                        
+
+                        string smtpFile = "c:\\SMTPINFO.txt";
+                        //SecureString gave some difficulties here would like to implement that though
+                        string SMTPPORT = File.ReadLines(smtpFile).Skip(0).Take(1).First();
+                        string SMTPHOSTURL = File.ReadLines(smtpFile).Skip(1).Take(1).First();
+                        string USERNAME = File.ReadLines(smtpFile).Skip(2).Take(1).First();
+                        string PASSWORD = File.ReadLines(smtpFile).Skip(3).Take(1).First();
+                        string FROMADDRESS = File.ReadLines(smtpFile).Skip(4).Take(1).First();
+                        string TOADDRESS = File.ReadLines(smtpFile).Skip(5).Take(1).First();
+
                         // SMTP Stuff Begin
                         SmtpClient client = new SmtpClient();
-                        client.Port = 587;
-                        client.Host = "SMTP-SERVER";
+                        client.Port = Int32.Parse(SMTPPORT);
+                        client.Host = SMTPHOSTURL;
                         client.EnableSsl = true;
 
                         client.Timeout = 10000;
                         client.DeliveryMethod = SmtpDeliveryMethod.Network;
                         client.UseDefaultCredentials = false;
-                        client.Credentials = new System.Net.NetworkCredential("SMTP-USERNAME", "SMTP-PW");
+                        client.Credentials = new System.Net.NetworkCredential(USERNAME, PASSWORD);
 
 
-                        MailMessage mm = new MailMessage("EMAIL-FROM", "EMAIL-TO", SNMPSubject, SNMPshortMsg);
+                        MailMessage mm = new MailMessage(FROMADDRESS, TOADDRESS, SNMPSubject, SNMPshortMsg);
                         mm.BodyEncoding = UTF8Encoding.UTF8;
                         mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
                         mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess;
@@ -141,6 +181,46 @@ namespace OpenITTools_SNMP_Listener_Emailer
 
                         client.Send(mm);
 
+                        try
+                        {
+                            // if the buffer file exists, send it and delete it
+                            if (File.Exists(bufferPath))
+                            {
+                                // read the file and delete it
+                                string bufferContents = File.ReadAllText(bufferPath);
+
+                                // byffer dump emailer Stuff Begin
+                                SmtpClient bufferDump = new SmtpClient();
+                                bufferDump.Port = Int32.Parse(SMTPPORT);
+                                bufferDump.Host = SMTPHOSTURL;
+                                bufferDump.EnableSsl = true;
+
+                                bufferDump.Timeout = 10000;
+                                bufferDump.DeliveryMethod = SmtpDeliveryMethod.Network;
+                                bufferDump.UseDefaultCredentials = false;
+                                bufferDump.Credentials = new System.Net.NetworkCredential(USERNAME, PASSWORD);
+
+
+                                MailMessage bufferMessage = new MailMessage(FROMADDRESS, TOADDRESS, DateTime.Now + " Offline Buffer Dump", bufferContents);
+                                bufferMessage.BodyEncoding = UTF8Encoding.UTF8;
+                                bufferMessage.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+                                bufferMessage.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess;
+                                bufferMessage.DeliveryNotificationOptions = DeliveryNotificationOptions.Delay;
+
+                                bufferDump.Send(bufferMessage);
+
+
+                            }
+
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Buffer File Send Failure!!!");
+                        }
+
+
+
+                        
                         try
                         {
 
@@ -165,6 +245,16 @@ namespace OpenITTools_SNMP_Listener_Emailer
                         catch
                         {
                             Console.WriteLine("Log File Failure");
+
+                            // create and log to the buffer file begin
+                            if (!File.Exists(bufferPath))
+                            {
+                                // Create a file to write to.
+                                using (StreamWriter sw = File.CreateText(bufferPath))
+                                {
+                                    sw.WriteLine(DateTime.Now + " ----- " + SNMPBody);
+                                }
+                            }
                         }
 
 
@@ -176,6 +266,24 @@ namespace OpenITTools_SNMP_Listener_Emailer
                         
                         Console.WriteLine(DateTime.Now + " - SNMP Email Failed ----- " + EXCEP + " ----- " + SNMPBody);
                         
+                        try
+                        {
+                            // create and log to the buffer file begin
+                            if (!File.Exists(bufferPath))
+                            {
+                                // Create a file to write to.
+                                using (StreamWriter sw = File.CreateText(bufferPath))
+                                {
+                                    sw.WriteLine(DateTime.Now + " ----- " + SNMPBody);
+                                }
+                            }
+
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Buffer File Failure!!!");
+                        }
+
                         try
                         {
                             
@@ -200,7 +308,7 @@ namespace OpenITTools_SNMP_Listener_Emailer
                         }
                         catch
                         {
-                            Console.WriteLine("Email Notifier Failure");
+                            Console.WriteLine("Email Notifier & Logging Failure");
                         }
 
                       
